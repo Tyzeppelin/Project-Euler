f722b0673cd147de606bf041f30b8e0228414f35
Tyzeppelin <boschet.francois@gmail.com>
2013-10-03 20:02:17 +0200

    problem10 solved

---
+++ problem10/problem10.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+
+from math import *
+from time import *
+
+# create a list of all prime number minor than n
+def genere (n) :
+
+	primeArray = [2]	
+	i = 3
+	while  i < n :
+		if div(i, primeArray) :
+			primeArray.append(i)
+			#print "add ", i
+		i += 1
+	print(primeArray)
+	return primeArray
+
+
+def div (n, tab) :
+	i = 0
+	while i < sqrt(len(tab)) :
+		if n % tab[i] == 0 :
+			return False
+		i += 1
+	return True
+
+
+
+def sumOfPrime (n) :
+	
+	sum = 0
+
+	tab = genere(n)
+	for i in tab:
+		sum += i
+
+	print sum
+
+
+if __name__ == "__main__" :
+
+	#print sum(genere(2000000))
+
+	t1 = clock()
+
+	sumOfPrime(2000000) # ^ same ^
+	
+	t2 = clock()
+	print "Execute in", t2-t1, "seconds."
