f02ac954c5f807348ca5a5b99d349f31bb1e7d5b
Tyzeppelin <boschet.francois@gmail.com>
2013-12-07 22:50:28 +0100

    issues with pb25 solved

---
+++ Nouveau dossier/problem25.py
@@ -0,0 +1,16 @@
+from math import *
+
+f0 = 0
+f1 = 1
+fn = 1
+
+j = 1
+
+while log10(fn)+1 < 1000 :
+	
+	fn = f0 + f1
+	f0 = f1
+	f1 = fn
+	j += 1
+
+print "The first 1000-digits term of fibonacci sequence is the", j,"th"
