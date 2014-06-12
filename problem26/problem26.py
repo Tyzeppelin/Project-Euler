d7579e08a9a4984faeb64efbc9070b8e0fa9b31e
Tyzeppelin <boschet.francois@gmail.com>
2014-06-13 00:54:54 +0200

    Pb26 done. I aad a function to test if a pattern is the real recurring cycle.

--- problem26/problem26.py
+++ problem26/problem26.py
@@ -3,26 +3,44 @@
 
 import time
 
+# Find if a pattern patt is a reccuring pattern in s
+def isRecCycle(s, patt):
+    banana = s
+    hamac = patt
+    while len(banana) > 0:
+        if banana[0] == hamac[0]:
+            banana = banana[1:]
+            hamac = hamac[1:] + hamac[0]
+        else :
+            return False
+    return True
 
-def countSubStr(s, patt):
+# find the number of substring patt is in s
+def countSubStr (s, patt):
     return s.count(patt)
+
+
+
+def reduceStr (s):
+    wip = s
+    while wip[-1] == '0':
+        wip = wip[:-1]
+    return wip
 
 
 # Find the largest recurring cycle in a number n
 # return -1 if none found
 def recurringCycle(n):
     i = 0
-    s = str(n)
+    s = reduceStr(str(n))
     sz = len(s)
     patt = ''
     max = -1
-    while (i < sz/2):
-
+    while i < sz/2:
         patt += s[i]
-        nbSus = countSubStr(s, patt)
-
-        if nbSus == sz/(i+1):
-            return len(patt)
+        if countSubStr(s, patt) == sz/(i+1):
+            if isRecCycle(s, patt):
+                return i+1
         i += 1
     return max
 
@@ -31,8 +49,8 @@ if __name__ == "__main__":
 
     t1 = time.clock()
 
-    PRECISION = 8000
-    num = pow(10, PRECISION)
+    PUISSANCE = 8000
+    num = pow(10, PUISSANCE)
     max = 1
     cycle = 0
     den = 1
@@ -41,10 +59,13 @@ if __name__ == "__main__":
         n = num / den
         rec = recurringCycle(n)
         if rec > max:
-            print "den ->", den, "max cycle ->", rec
+            # print "den ->", den, "max cycle ->", rec
             max = den
             cycle = rec
         den += 1
 
     print "maximum recurring cycle -> max", max, " ; cycle", cycle
+
+
+
     print "Execute in", time.clock()-t1, "seconds"
