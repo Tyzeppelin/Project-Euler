c0faabf5fc03e6779e63ec805e470568e394b0cd
Tyzeppelin <boschet.francois@gmail.com>
2014-07-07 11:57:31 +0200

    problem31, trying to implements an algorithm

--- problem31/problem31.py
+++ problem31/problem31.py
@@ -6,32 +6,55 @@ import time
 def insert(l, c):
 #   print l, c
     if l == [[]] or l is None:
-        return l
+        return [c]
     arr = l
     for e in arr:
         e.append(c)
     return arr
 
 # A modifier pour prendre en compte les valeurs de `currency`
-#def part(n, k):
-#    if n == 0 and k == 0:
-#        return [[]]
-#    elif k == 0:
-#        return []
-#    elif k > n:
-#        return part(n, n)
-#    else:
-#        return insert(part(n-k, k), k).append(part(n, k-1))
+def part(n, k):
+    if n == 0 and k == 0:
+        return case1()
+    elif k == 0:
+        return case2()
+    elif k > n:
+        return case3(n)
+    else:
+        n = case4(n-k, k)
+        m = case4(n, k-1)
+        return insert(n, k).append(m)
+
+
+def case1():
+    return [[]]
+
+def case2():
+    return []
+
+def case3(n):
+    return part(n, n)
+
+def case4(k, n):
+    if k == 0 and n == 0:
+        return case1()
+    elif k ==0:
+        return case2()
+    elif k > n:
+        return case3(n)
+    else:
+        return part(k, n)
+
 
 
 if __name__ == "__main__":
     t1 = time.clock()
 
-    #currency = [1, 2, 5, 10, 20, 50, 100, 200]
+    currency = [1, 2, 5, 10, 20, 50, 100, 200]
 
     a = [[], [1], [2,3,4]]
     print insert(a, 4)
 
-    # print part(5, 5)
+    print part(5, 5)
 
     print "Completed in", time.clock()-t1, "seconds"
