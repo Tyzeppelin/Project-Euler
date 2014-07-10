7a2d84b818f2f8a22518a53d562ef9226878508f
Tyzeppelin <boschet.francois@gmail.com>
2014-07-10 18:22:39 +0200

    problem 31 solved in 1.34s.\n Special thanks to Alan Schmitt and the 6th tp of ocaml.

--- problem31/problem31.py
+++ problem31/problem31.py
@@ -3,47 +3,28 @@ import time
 # TODO : Regarder la fin du tp6 d'ocaml. Il y a un algo de partition
 
 
-def insert(l, c):
+def nextCurrency(c):
 #   print l, c
-    if l == [[]] or l is None:
-        return [c]
-    arr = l
-    for e in arr:
-        e.append(c)
-    return arr
+    curr_ptr = currency.index(c)
+    if curr_ptr == 0 :
+        return 0
+    else :
+        return currency[curr_ptr-1]
 
 # A modifier pour prendre en compte les valeurs de `currency`
 def part(n, k):
     if n == 0 and k == 0:
-        return case1()
+        #print "P(",n,",",k,")"
+        return 1
     elif k == 0:
-        return case2()
+        #print "P(",n ,",",k,")"
+        return 0
     elif k > n:
-        return case3(n)
+        #print "P(",n ,",",k,")", "P(",n ,",",n,")"
+        return part(n, n)
     else:
-        n = case4(n-k, k)
-        m = case4(n, k-1)
-        return insert(n, k).append(m)
-
-
-def case1():
-    return [[]]
-
-def case2():
-    return []
-
-def case3(n):
-    return part(n, n)
-
-def case4(k, n):
-    if k == 0 and n == 0:
-        return case1()
-    elif k ==0:
-        return case2()
-    elif k > n:
-        return case3(n)
-    else:
-        return part(k, n)
+        #print "P(",n ,",",k,")","&&","P(", n-k, ",", k, ") &&", "P(", n, ",",nextCurrency(k),")"
+        return part(n-k, k) + part(n, nextCurrency(k))
 
 
 
@@ -51,10 +32,12 @@ if __name__ == "__main__":
     t1 = time.clock()
 
     currency = [1, 2, 5, 10, 20, 50, 100, 200]
+    #currency = [1,2,3,4,5,6]
 
-    a = [[], [1], [2,3,4]]
-    print insert(a, 4)
+#    a = [[], [1], [2,3,4]]
+#    print insert(a, 4)
 
-    print part(5, 5)
-
+    #print part(6, currency[-1])
+    a =  part(200, currency[-1])
+    print a
     print "Completed in", time.clock()-t1, "seconds"
