e73b98c25139f25cdb265d8db10dad5a2eb1e6ff
Tyzeppelin <boschet.francois@gmail.com>
2014-07-29 16:35:58 +0200

    Resolution probleme 37 sur le temps de pause. Rapide. Plus ou moins le meme probleme que le 35

---
+++ problem37/problem37.py
@@ -0,0 +1,66 @@
+
+from math import *
+import time
+
+OPTIMUS = [2]
+
+#Remove digits from left to right
+def testLeft(n):
+    t = n
+    while t >= 1:
+        if not isPrime(t):
+            return False
+        t = int(t%(10**int(log10(t))))
+    return True
+
+#Remove digits from right to left
+def testRight(n):
+    t = n
+    while t >= 1:
+        if not isPrime(t):
+            return False
+        t = int(t/10)
+    return True
+
+
+def isPrime (n):
+    if n < 2:
+        return False
+    for prime in OPTIMUS:
+        if prime > sqrt(n) :
+            break
+        if n%prime == 0:
+            return False
+    return True
+
+# To make OPTIMUS greater
+def MatrixOfLeadership (n):
+    i = 2
+    while i < sqrt(n):
+        isP = True
+        for prime in OPTIMUS:
+            if i%prime == 0:
+                isP = False
+                break
+        if isP:
+            OPTIMUS.append(i)
+        i+=1
+
+if __name__ == "__main__" :
+
+    time1 = time.clock()
+
+    MatrixOfLeadership(1000000)
+    #print OPTIMUS
+
+    ct = 0
+    tab = []
+
+    for num in range(1000000)[9:]:
+        if (isPrime(num)):
+            if testLeft(num) and testRight(num):
+                ct += 1
+                tab.append(num)
+
+    print ct, tab, sum(tab)
+    print "Prime generation + computation under", time.clock()-time1, "seconds"
