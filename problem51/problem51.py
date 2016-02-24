5933e5c1bea599ab6bfbb8d09d954b969b870aaa
Francois Boschet <fboschet@insa-rennes.fr>
2016-02-24 19:21:25 +0000

    get the windosw work back

--- problem51/problem51.py
+++ problem51/problem51.py
@@ -1,61 +1,110 @@
 #!/usr/bin/python
 
+from math import *
+from collections import defaultdict
 import time
-import math
 
-OPTIMUS = [2,3]
 
-def bumblebee (n):
-    if OPTIMUS[-1] > n :
-        return
-
-    i = OPTIMUS [-1]
+def bumblebee(n):
     prime = True
-    while i < n :
+    OPTIMUS = [2, 3]
+    i = OPTIMUS[-1]
+    while OPTIMUS[-1] < n:
         prime = True
-        for p in OPTIMUS :
-            if i % p == 0 :
+        for p in OPTIMUS:
+            if i % p == 0:
                 prime = False
                 break
-        if prime :
-            OPTIMUS.append(map(int ,str(i)))
+        if prime:
+            OPTIMUS.append(i)
         i += 2
+    #return [[int(x) for x in str(m)] for m in OPTIMUS]
+    return [p for p in OPTIMUS if  p > 10000]
 
-def isPrime(n):
-    return OPTIMUS.contains(n) > 0
-
-
-def intlen(i):
-    return math.ceil(math.log10(i))
-
-def have8Family(p, seq):
-    None
-
-
-# p   -> prime number as an array
-# seq -> sequence
-def eightFamily(p, seq):
-    if p == seq:
-        return False
-    elif have8Family(p, seq):
-        return True
-    elif seq[-1] == (len(p)-1):
-        return ""
-
-if __name__ == "__main__" :
+def isPrime(primes, n):
+    end = int(sqrt(n))
+    i = 0
+    while primes[i] < end:
+        if n%primes[i] == 0:
+            return False
+        i += 1
+    return True
 
 
+def mask(n):
+    return [1] * n
+
+
+def allMasks(m, i):
+    if i == 0:
+        return []
+    else :
+        m0 = m[:]
+        m0[i-1] = 0
+        return [m0]+allMasks(m0, i-1)+allMasks(m[:], i-1)
+
+
+def family(n, mask, domain):
+    fam = 0
+    w = zip([int(x) for x in str(n)], mask)
+    for e in range(10):
+        maybe = [n if m == 1 else e for m, n in w]
+        if int(''.join(map(str, maybe))) in domain:
+            fam += 1
+    return fam
+
+
+def match(n, mask):
+    same = [e for e, m in zip([int(x) for x in str(n)], mask) if m == 0]
+    return all(same[0] == e for e in same)
+
+
+def unmask(n, mask):
+    return int(''.join(map(str, [e for e, m in zip([int(x) for x in str(n)], mask) if m == 1])))
+
+if __name__ == "__main__":
     t1 = time.clock()
 
-    bb = 10000
-    bumblebee(bb)
+    optimus = bumblebee(99990)
+    print len(optimus), "primes from", optimus[0], "to", optimus[-1], "generated under", time.clock()-t1, "seconds"
+    families = defaultdict(lambda: 0)
+    currMasks = [am for am in allMasks(mask(5), 5) if am.count(0) == 2 or am.count(0) == 3]
+    print len(currMasks), currMasks
 
-    i = 4
-    smallest = -1
+    print "==========="
 
-    while smallest == -1 :
-        if eightFamily(OPTIMUS[i], [OPTIMUS[0]]):
-            smallest = int(''.join(map(str,OPTIMUS[i])))
+    matches = defaultdict(lambda: [])
+    i = 0
+    op = 0
+    for m in currMasks:
+        matches[i] = defaultdict(lambda: 0)
+        for p in optimus:
+            if match(p, m):
+                matches[i][unmask(p, m)] += 1
+            op += 1
+        for k, v in matches.items():
+            if v == 8:
+                print "8 ->", k, "mask ->", k
         i += 1
+    print "==========="
+    for index in matches.keys():
+        print currMasks[index], matches[index]
+    print "==========="
+    print op, "operations"
 
-    print "done under", time.clock() - t1, "seconds"
+    # for p in optimus:
+    #     for mm in currMasks:
+    #         if p == 56003:
+    #             print p, mm, family(p, mm, optimus)
+    #         f = family(p, mm, optimus)
+    #         families[f] += 1
+    #         if f == 8:
+    #             print p, mm
+    #             break
+    #     else:
+    #         continue
+    #     break
+
+    print families
+
+    print "executed under", time.clock() - t1, "seconds"
