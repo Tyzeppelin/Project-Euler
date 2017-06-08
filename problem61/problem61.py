1a0049b9e4e61b775a8af2a25efb0a3fe75d0cea
Boschet Francois <francois.boschet@crf.canon.fr>
2017-06-08 14:50:28 +0200

    pb61, dfs-ish o/

--- problem61/problem61.py
+++ problem61/problem61.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+import time
+
+from collections import defaultdict
 from math import ceil, log, log10, sqrt
 
 def tria(n):
@@ -32,14 +35,42 @@ def octo(n):
 def is_oct(n):
     return (1+sqrt(1+3*n))/3 %1 == 0.0
 
+
+   
 if __name__ == "__main__":
 
-    o = [n for n in [octo(i) for i in range(1,200)] if ceil(log10(n)) == 4]
-    h = [n for n in [hept(i) for i in range(1,200)] if ceil(log10(n)) == 4]
-    x = [n for n in [hexa(i) for i in range(1,200)] if ceil(log10(n)) == 4]
-    p = [n for n in [pent(i) for i in range(1,200)] if ceil(log10(n)) == 4]
-    s = [n for n in [squa(i) for i in range(1,200)] if ceil(log10(n)) == 4]
-    t = [n for n in [tria(i) for i in range(1,200)] if ceil(log10(n)) == 4]
-    print(o, h, x, p, s, t)
+    t1 = time.clock()
 
+    o = [(n/100, n%100) for n in [octo(i) for i in range(1,800)] if ceil(log10(n)) == 4]
+    h = [(n/100, n%100) for n in [hept(i) for i in range(1,800)] if ceil(log10(n)) == 4]
+    x = [(n/100, n%100) for n in [hexa(i) for i in range(1,800)] if ceil(log10(n)) == 4]
+    p = [(n/100, n%100) for n in [pent(i) for i in range(1,800)] if ceil(log10(n)) == 4]
+    s = [(n/100, n%100) for n in [squa(i) for i in range(1,800)] if ceil(log10(n)) == 4]
+    t = [(n/100, n%100) for n in [tria(i) for i in range(1,800)] if ceil(log10(n)) == 4]
     
+    #print(len(o),len(h),len(x),len(p),len(s),len(t))
+
+    sets = {0:o, 1:h, 2:x, 3:p, 4:s, 5:t}
+    nums = [(item, key) for (key, itemlist) in sets.iteritems() for item in itemlist]
+    where = []
+
+    def next(v):
+        return [a for a in nums if a[1] not in where and v[0][1] == a[0][0] and a != v]
+
+    def dfs(v, depth, acc):
+        #print(depth, len(where), acc, "--", where)
+        if depth == 6:
+            if acc[0][0][0] == acc[-1][0][1]:
+                print(acc, sum([100*a[0]+a[1] for a,_ in acc]))
+        else:
+            for w in next(v):
+                where.append(w[1])
+                dfs(w, depth+1, acc+[w])
+                where.pop()
+    
+    for x in nums:
+        where.append(x[1])
+        dfs(x, 1, [x])
+        where.pop()
+    
+    print(time.clock() - t1, "seconds")
