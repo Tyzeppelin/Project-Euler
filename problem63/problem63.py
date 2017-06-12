e1a4b86330826a3ae0ba52bf82e48b214a9280bb
Boschet Francois <francois.boschet@crf.canon.fr>
2017-06-12 11:15:33 +0200

    pb63, was a pb of computing length of int

--- problem63/problem63.py
+++ problem63/problem63.py
@@ -12,11 +12,11 @@ if __name__ == "__main__":
     for i in range(1, 50):
         cl = 0
         for n in range(1,10):
-            if ceil(log10(n**i)) == i:
-                print(i, n**i)
+            if int(log10(n**i))+1 == i:
+                print(n, "**", i, n**i)
                 c += 1
                 cl += 1
-        print(i, cl)
+        #print(i, cl)
 
     print(c)
     print(time.clock() - t1, "seconds")
