d9ee4d175fbf2662e984b7ba6f707b80fd2e3f55
Boschet Francois <francois.boschet@crf.canon.fr>
2017-06-14 10:10:17 +0200

    pb67 non greedy + fast

--- problem67/problem67.py
+++ problem67/problem67.py
@@ -1,19 +1,16 @@
-#!/usr/bin/env/python
 
-import sys
 import time
 
-if __name__ == "__main__" :
+
+if __name__ == "__main__":
 
     t1 = time.clock()
 
-    with open(sys.argv[1], 'r') as f:
-        lines = [[int(d) for d in e] for e in [a.split(" ") for a in f][:-1]]
+    with open("triangle.txt", 'r') as f:
+        triangle = [map(int, s.split(" ")) for s in f.readlines()]
 
-    l = len(lines)
+    for i in reversed(range(1, len(triangle), 1)):
+        triangle[i-1] = [triangle[i-1][j] + max(triangle[i][j], triangle[i][j+1]) for j in range(0,len(triangle[i-1]))] 
 
-    for i in range(l-1, 0, -1):
-        lines[i-1] = [lines[i-1][j]+max(lines[i][j], lines[i][j+1]) for j in range(len(lines[i-1]))]
-        #print(lines[i-1]) 
-    print(lines[:3])
+    print(triangle[0][0])
     print(time.clock() - t1, "seconds")
