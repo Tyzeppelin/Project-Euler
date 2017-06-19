676cf521ece7f480af8c06149c1d1a5f2332d216
Boschet Francois <francois.boschet@crf.canon.fr>
2017-06-19 11:41:08 +0200

    pb78 bruteforce method shows its limits had to be smarter
    I used the pentagonal theorem that compute the number of partitions
    of a set iterating over pentagonal numbers (instead of naturals).
    It's 100 times faster for computing partitions under 1000. Thx
    wikipedia and wolrfam for the formula. The rest, they say, is
    dynamic programming.

--- problem78/problem78.py
+++ problem78/problem78.py
@@ -1,8 +1,10 @@
 
+import math
 import sys
 import time
 
 from collections import defaultdict
+from math import sqrt
 
 def dynamic(f):
     cache = defaultdict(lambda: -1)
@@ -12,30 +14,62 @@ def dynamic(f):
         return cache[args]
     return is_known
 
+#@dynamic
+#def part(n, k):
+#    if n == 0 and k == 0:
+#        return 1
+#    if k == 0:
+#        return 0
+#    elif k > n:
+#        return part(n, n)
+#    else:
+#        return part(n-k, k) + part(n, k-1)
+
+
 @dynamic
-def part(n, k):
-    if n == 0 and k == 0:
+def part(n):
+    if n == 1 or n == 0:
+        #print('hit')
         return 1
-    if k == 0:
-        return 0
-    elif k > n:
-        return part(n, n)
-    else:
-        return part(n-k, k) + part(n, k-1)
+    
+    s = 0
+    k = 1
+    p = (3*k**2-k)/2
+
+    while p <= n:
+        #print(n,k,p,s)
+        if k % 2 == 0:
+            s -= part(n - p)
+            if (p + k) <= n:    
+                s -= part(n - p - k)
+        else:
+            s += part(n - p)
+            if (p + k) <= n:
+                s += part(n - p - k)
+        k += 1
+        p = (3*k**2-k)/2
+    return s
 
 
 if __name__ == "__main__":
 
-    sys.setrecursionlimit(1000000)
-
     t1 = time.clock()
 
-    #print(part(330,330))
-    i = 1
-    while len(str(part(i, i))) <  100:
-        print(i, part(i,i))
-        i += 1
+    #r = []
+    #t2 = time.clock()   
+    #for i in range(1000):
+    #    r.append(part(i,i))
+    #print("part1 : ", time.clock() - t2, "seconds")
+
+    #r = []
+    #t2 = time.clock()
+    #for i in range(1000):
+    #    r.append(part2(i))
+    #print("part2 : ", time.clock() - t2, "seconds")
     
-    print(i, part(i,i))
+    i = 1
+    while part(i) % 1000000 != 0:
+        i += 1
+    print(i, part(i))
 
     print(time.clock() - t1, "seconds")
