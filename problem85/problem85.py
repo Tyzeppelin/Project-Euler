ffbba14c2db2739f6feb01528a286a6689560bb0
François Boschet <francois@boschet.io>
2018-05-29 19:27:16 +0200

    pb85 minor fix. 0.1s

--- problem85/problem85.py
+++ problem85/problem85.py
@@ -1,7 +1,7 @@
 
 import time
 
-from math import inf
+from math import inf,sqrt
 
 if __name__ == "__main__":
 
@@ -14,8 +14,8 @@ if __name__ == "__main__":
     distance = inf
     surface = 0
 
-    for i in range(10000)[1:]:
-        for j in range(1000)[1:]:
+    for i in range(int(sqrt(target)))[1:]:
+        for j in range(int(sqrt(target)))[1:]:
             n = nb_rect_line(i)
             m = nb_rect_line(j)
             if n*m > target:
