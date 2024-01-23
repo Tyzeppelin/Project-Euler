dea31cc1ea737595da6dc9146b33b4029c90195b
François Boschet <boschet.francois@gmail.com>
2024-01-23 13:28:24 +0100

    6 years later, problem 88 solved

--- problem88/problem88.py
+++ problem88/problem88.py
@@ -1,8 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import collections
-import functools
-import itertools
+import math
 import time
+
+# max N = 36481
+OPTIMUS = (2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191,)
+
 
 def dynamic(f):
     cache = collections.defaultdict(lambda: [])
@@ -14,89 +19,63 @@ def dynamic(f):
     return is_known
 
 
-@dynamic
-def divisors(n, k):
-    res = []
-    for e in range(2, max(n, n//4+1)):
-        if n%e == 0:
-            if (e <= k):
-                res.append(e)
-            if (n/e <= k):
-                res.append(n/e)
-    return res
-
-#@dynamic
-def combinations_with_limit(divs, r, limit):
-    # combinations_with_replacement('ABC', 2) --> AA AB AC BB BC CC
-    pool = tuple(divs)
-    n = len(pool)
-    if not n and r:
-        return
-    indices = [0] * r
-    yield tuple(pool[i] for i in indices)
-    while True:
-        for i in range(r, 0, -1):
-            if indices[i] != n - 1:
-                break
-        else:
-            return
-        indices[i:] = [indices[i] + 1] * (r - i)
-        if functools.reduce(lambda x, a: x*a, (pool[i] for i in indices)) == limit:
-            yield tuple(pool[i] for i in indices)
 
 @dynamic
-def dyn_combinations(n, r):
-    return itertools.combinations_with_replacement(n, r)
+def divisors(n):
+    a = math.ceil(math.sqrt(n))
+
+    ret = []
+
+    if n%2==0:
+        ret += [list([2, n//2])]
+        if n//2 not in OPTIMUS:
+            divs = divisors(n//2)
+            z = [ds+[2] for ds in divs]
+            ret += z
+
+    for p in range(3, a+1):
+        if p>a:
+            break
+        if n%p == 0:
+            ret += [list([p, n//p])]
+            if n//p not in OPTIMUS:
+                divs = divisors(n//p)
+                z = [ds+[p] for ds in divs]
+                ret += z
+    return ret
 
 
 def is_product_sum(n, k):
-    div = divisors(n, k+1)
-    #print("n", n, "div", div)
-
-    for j in range(2, k+1):
-        if (len(div) < j):
-            break
-        #cs = combinations_with_limit(div, j, n)
-        cs = dyn_combinations(div, j)
-        #print("j cs", j, list(cs))
-        for comb in cs:
-            s = sum(comb)
-            # print("comb j s k", comb, j, s, k)
-            #if s + k-j == n and functools.reduce(lambda x, a: x*a, comb) == n:
-            if s + k-j - functools.reduce(lambda x, a: x*a, comb) == 0:
-            #if s + k-j == n:
-                #print(n, div, comb, k, j)
+    divs = divisors(n)
+    for ds in divs:
+        if len(ds) <= k:
+            if sum(ds) + k - len(ds) == n:
                 return True
     return False
 
 
-def smallest_product_sum(k):
-    k = 2
-    for e in range(4, 30):
-        #
-        pass
-
-
 if __name__ == "__main__":
-
-    t1 = time.perf_counter()
-
-    TEST_BREAK = 100
+    t1 = time.time()
 
     k = 2
     n = 4
 
-    test_res = [0, 0, 4, 6, 8, 8, 12, 12, 12, 15, 16, 16, 16, 18, 20, 24, 24, 24, 24, 24, 28, 27, 32, 30, 48, 32]
+    MAX_K = 12000
+    MAX_N = 36481
 
-    while k <= len(test_res)  and n < TEST_BREAK:
-        while n < TEST_BREAK:
+    r = []
+
+    while k <= MAX_K and n < MAX_N:
+        n = k
+        while n < MAX_N:
             if is_product_sum(n, k):
-                print("===k:", k, "; n:", n, "; divsize", len(divisors(n, k+1)))
-                n = min(k,n)
-                print("new n", n)#, "test n", test_res[n])
+                r.append(n)
                 k += 1
+                break
             else:
-                #print("nope ->", n, "; divsize", len(divisors(n)))
                 n += 1
+    
+    print("len", len(set(r)))
+    print("sum", sum(set(r)))
 
-    print("Done under", time.perf_counter() - t1, "seconds")
+    print("done under", time.time() - t1, "seconds")
